841918e1e681b18457a83a9793654535777bd8a8
Connor Lawless <connor.lawless@hotmail.com>
2016-06-21 13:15:17 +0200

    Now outputs a data frame with score, lat and long!

--- city_match.R
+++ city_match.R
@@ -1,11 +1,10 @@
 library(dplyr)
 getCities <- function(weights, df.subset){
   scores <- apply(df.subset, 1, FUN = function(x){
-    tmp <- x[-c(1,9,10)]
+    tmp <- x[-c(1,9,10,16,17)]
     score <- as.numeric(tmp)*weights
     score <- sum(score)
     score
   })
-  names(scores) <- df.subset[,1]
-  
+  scores.df <- data.frame(City = df.subset[,1], Score = scores, Lon = df.subset["lon"], Lat = df.subset["lat"])
 }
